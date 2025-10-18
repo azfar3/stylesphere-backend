456f71ca004ffb4b52cb882770d2a763023a985b
azfar3 <azfarmurtaza5@gmail.com>
2025-10-18 19:55:06 +0500

    Camparison Page Updated

--- backend/routes/priceComparisonRoutes.js
+++ backend/routes/priceComparisonRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getComparisonData, getPriceTrends } from '../controllers/priceComparisonController.js';
+import {
+    getComparisonData,
+    getPriceTrends,
+    getProductTypes,
+} from '../controllers/priceComparisonController.js';
 
 const router = express.Router();
 
 router.get('/', getComparisonData);
 router.get('/trends/:productId', getPriceTrends);
+router.get('/product-types', getProductTypes);
 
 export default router;
